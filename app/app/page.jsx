462c3b318dd79471d2b46b1747501c1cacfe744a
AnkitLabelNest <ankit@labelnest.in>
2025-12-26 23:15:17 +0530

    Delete app/app directory

--- app/app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>AnnoNest – NestAnnotate</h1>
-      <p>External execution UI is live.</p>
-    </div>
-  );
-}
